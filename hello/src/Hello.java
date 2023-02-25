3cc276121815ba7e8244de7434ecd49df8ca4b61
psh01 <psh01@192.168.219.100>
2023-02-25 18:50:54 +0900

    .

--- hello/src/Hello.java
+++ hello/src/Hello.java
@@ -2,7 +2,10 @@
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("왜 안되는지 모르겠다");
+		int[] s = new int[10];
+		
+		for(int value : s)
+			System.out.println(value);
 		
 	}
 
