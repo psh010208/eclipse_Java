7d9e8a446a5e3db7504b3abf58e24e2dfc47458c
박상현 <psh010208@naver.com>
2023-02-28 19:40:16 +0900

    .

---
+++ sibal/src/Sibal.java
@@ -0,0 +1,43 @@
+class sisibal {
+	
+	String color;
+	int gear;
+	int speed;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Car [color=" + color + ", gear=" + gear + ", speed=" + speed + "]";
+	}
+
+	void setColor(String c) {
+		color = c;
+	}
+	
+	void changeGear(int g) {
+		gear = g;
+	}
+
+	void speedUp() {
+		speed = speed + 1;
+	}
+	
+	void speedDown() {
+		speed = speed - 1;
+	}
+	
+}
+public class Sibal {
+
+	public static void main(String[] args) {
+		
+		sisibal car = new sisibal();
+		car.setColor("»¡°­»ö");
+		car.changeGear(2);
+		car.speedUp();
+		System.out.println(car);
+			
+	}
+
+}
