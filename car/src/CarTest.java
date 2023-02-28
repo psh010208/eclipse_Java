cb519d0f5ccd3b9e42ddd7ad52faf2bbd8b29dcc
박상현 <psh010208@naver.com>
2023-02-28 20:38:50 +0900

    .

--- car/src/CarTest.java
+++ car/src/CarTest.java
@@ -4,7 +4,7 @@ public class CarTest {
 	public static void main(String[] args) {
 		
 		Car car = new Car();
-		car.setColor("»¡°­");//
+		car.setColor("»¡°­");//ÇÑ±Û ±úÁü °íÄ¡±â
 		car.changeGear(2);
 		car.speedUp();
 		System.out.println(car);
