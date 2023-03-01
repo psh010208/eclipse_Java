4937b50eb8f0a72ebfc4ea9441af3a2372a73e86
박상현 <psh010208@naver.com>
2023-03-01 19:23:55 +0900

    .

--- account/src/Account.java
+++ account/src/Account.java
@@ -4,20 +4,26 @@ public class Account {
 	private String name;
 	private int balance;
 	
+	//Source -> Generate Getters and Setters를 이용하면 필드를 지정해서 자동으로 설정자와 접근자를 생성해줌
+	public int getRegNumber() {
+		return regNumber;
+	}
+	public void setRegNumber(int regNumber) {
+		this.regNumber = regNumber;
+	}
 	public String getName() {
 		return name;
 	}
-	
 	public void setName(String name) {
 		this.name = name;
 	}
-	
 	public int getBalance() {
 		return balance;
 	}
-	
 	public void setBalance(int balance) {
 		this.balance = balance;
 	}
+	
+	
 
 }
--- account/src/AccountTest.java
+++ account/src/AccountTest.java
@@ -3,7 +3,9 @@ public class AccountTest {
 
 	public static void main(String[] args) {
 		Account obj = new Account();
-		
+		obj.setName("Tom");
+		obj.setBalance(10000);
+		System.out.println("이름은 " + obj.getName() + " 통장 잔고는 " + obj.getBalance() + " 입니다");
 
 	}
 
