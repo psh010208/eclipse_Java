74937a74ae23417e6c11085cffd7ee5dc9106ae7
박상현 <psh010208@naver.com>
2023-03-03 18:20:36 +0900

    .

---
+++ employee/src/Employee.java
@@ -0,0 +1,25 @@
+
+public class Employee {
+
+	private String name;
+	private double salary;
+	
+	private static int count = 0; // 정적 변수
+	
+	// 생성자
+	public Employee(String n, double s) {
+		name = n;
+		salary = s;
+		count++; // 정적 변수인 count를 증가
+	}
+	
+	// 객체가 소멸될 때 호출
+	public void finalize() {
+		count--; // 직원이 하나 줄어드므로 count를 하나 감소
+	}
+	
+	//정적 메소드
+	public static int getCount() {
+		return count;
+	}
+}
