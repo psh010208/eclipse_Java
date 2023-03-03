44674a75f563ab3629c060c4400e701f92b91661
박상현 <psh010208@naver.com>
2023-03-03 13:28:15 +0900

    .

--- box/src/Box.java
+++ box/src/Box.java
@@ -13,11 +13,18 @@ public class Box {
 		volume = width * length * height;
 	}
 
-	Box whosLargest(Box box1, Box box2) {
+	Box whosLargest(Box box1, Box box2) { // 객체의 부피를 비교해서 큰 객체의 부피를 반환
 		if(box1.volume > box2.volume)
 			return box1;
 		else
 			return box2;
+	}
+	
+	boolean isSameBox(Box obj) { // 객체의 필드를 비교해서 boolean값을 반환
+		if((obj.width == width) && (obj.length == length) && (obj.height == height))
+			return true;
+		else
+			return false;
 	}
 
 
