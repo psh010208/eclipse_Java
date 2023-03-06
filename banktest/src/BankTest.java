5aa3a74b84101b62a14b14260f01377c42838896
박상현 <psh010208@naver.com>
2023-03-06 13:08:39 +0900

    .

--- banktest/src/BankTest.java
+++ banktest/src/BankTest.java
@@ -40,7 +40,7 @@ public class BankTest{
 		NormalBank b2 = new NormalBank();
 		GoodBank b3 = new GoodBank();
 		
-		System.out.println("BadBank의 이자율 : " + b1.getInterestRate());
+		System.out.println("Bad의 이자율 : " + b1.getInterestRate());
 		System.out.println("NormalBank의 이자율 : " + b2.getInterestRate());
 		System.out.println("GoodBank의 이자율 : " + b3.getInterestRate());
 	}
