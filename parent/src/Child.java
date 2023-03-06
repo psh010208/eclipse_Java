7f210ebf23d876cd266cd632fcdefc2bf1b66101
박상현 <psh010208@naver.com>
2023-03-06 12:13:33 +0900

    .

---
+++ parent/src/Child.java
@@ -0,0 +1,17 @@
+
+public class Child extends Parent{
+
+	@Override
+	public void print() {
+		super.print();
+		System.out.println("자식 클래스의 print() 메소드");
+	}
+	
+	public static void main(String[] args) {
+		
+		Child obj = new Child();
+		obj.print();
+
+	}
+
+}
