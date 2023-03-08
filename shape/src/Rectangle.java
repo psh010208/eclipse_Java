e158901f73ba756e425c8480fb90c9dca6f3ee92
박상현 <psh010208@naver.com>
2023-03-08 19:53:16 +0900

    .

--- shape/src/Rectangle.java
+++ shape/src/Rectangle.java
@@ -1,18 +1,18 @@
-
-public class Rectangle extends Shape {
-
-	private int width;
-	private int height;
-	
-	public Rectangle(int x, int y, int width, int height) {
-		super(x, y);
-		System.out.println("Ractengle()");
-		this.width = width;
-		this.height = height;
-	}
-	
-	double calcArea() {
-		return width * height;
-	}
-	
-}
+//
+//public class Rectangle extends Shape {
+//
+//	private int width;
+//	private int height;
+//	
+//	public Rectangle(int x, int y, int width, int height) {
+//		super(x, y);
+//		System.out.println("Ractengle()");
+//		this.width = width;
+//		this.height = height;
+//	}
+//	
+//	double calcArea() {
+//		return width * height;
+//	}
+//	
+//}
---
+++ shape/src/ShapeTest.java
@@ -0,0 +1,12 @@
+
+public class ShapeTest {
+
+	public static void main(String[] args) {
+		
+			Circle obj = new Circle();
+			obj.draw();
+			Rectangle obj2 = new Rectangle();
+			obj2.draw();
+	}
+
+}
