a53cf961292038faca0331ec40749735e4258527
박상현 <psh010208@naver.com>
2023-03-08 22:25:56 +0900

    .

---
+++ cartest/src/CarTest.java
@@ -0,0 +1,44 @@
+//
+//class Car {
+//	
+//}
+//
+//public class CarTest {
+//
+//	public static void main(String[] args) {
+//		Car obj = new Car();
+//		System.out.println("obj is of type " + obj.getClass().getName());
+//
+//	}
+//
+//}
+
+class Car{
+	private String model;
+
+	public Car(String model) {
+		this.model = model;
+	}
+
+
+	public boolean equals(Object obj) {
+		if(obj instanceof Car)
+			return this.model.equals( ((Car)obj).model);
+		else
+			return false;
+	}
+}
+
+public class CarTest{
+	public static void main(String args[]) {
+		
+		Car firstCar = new Car("HMW520");
+		Car secondCar = new Car("HMW520");
+		
+		if(firstCar.equals(secondCar))
+			System.out.println("동일한 종류의 자동차입니다");
+		else
+			System.out.println("다른 종류의 자동차입니다.");
+
+	}
+}
