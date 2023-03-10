02981a4103fd923c096078864187d7653407f6cf
박상현 <psh010208@naver.com>
2023-03-10 21:40:06 +0900

    .

--- book/src/Book.java
+++ book/src/Book.java
@@ -18,4 +18,5 @@ public class Book {
 		System.out.println(mybook);
 	}
 
+	
 }
--- typetest1/src/TypeTest1.java
+++ typetest1/src/TypeTest1.java
@@ -30,7 +30,7 @@ public class TypeTest1 {
 	public static void main(String[] args) {
 		A obj = new B(); // 부모 클래스의 참조 변수로 자식 클래스의 객체를 참조할 수 있다!
 		obj.draw();
-//		obj.speak() obj가 A타입의 참조 변수이기 때문에 A에서 상속받지 못한 speak() 메소드는 사용 불가
+//		obj.speak() obj가 A타입의 참조 변수이기 때문에 A에서 상속받지 못한 speak() 메소드는 사용 불가능
 	}
 
 }
