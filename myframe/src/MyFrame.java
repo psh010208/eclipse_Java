b6c871618d24484dbc14e65f0972ff15ea30faf2
박상현 <psh010208@naver.com>
2023-03-13 23:44:24 +0900

    .

--- myframe/src/MyFrame.java
+++ myframe/src/MyFrame.java
@@ -14,7 +14,7 @@ public class MyFrame extends JFrame{
 		setLocation(screenSize.width / 2, screenSize.height / 2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("MyFrame");
-		Image img = kit.getImage("./icon.gif");
+		Image img = kit.getImage("icon.gif");
 		setIconImage(img);
 		setLayout(new FlowLayout());
 		JButton button = new JButton("¹öÆ°");
