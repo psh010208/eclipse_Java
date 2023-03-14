1ca051e79dd41a948fe972b98c16338920630d5f
박상현 <psh010208@naver.com>
2023-03-14 22:03:19 +0900

    .

--- myframe2/src/MyFrame2.java
+++ myframe2/src/MyFrame2.java
@@ -1,3 +1,5 @@
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
---
+++ myframe3/src/GridTest.java
@@ -0,0 +1,9 @@
+
+public class GridTest {
+
+	public static void main(String[] args) {
+		
+		MyFrame3 f = new MyFrame3();
+	}
+
+}
